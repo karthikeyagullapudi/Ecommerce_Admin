dae6a924d58bc4f1e8d30d10030bd55da2670daa
karthikeyagullapudi <karthikgullapudi@gmail.com>
2025-06-03 10:56:15 +0530

    designed admin page

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,16 +1,13 @@
 const SideBar = () => {
   return (
-    <>
-      <div className="SideBar">
-        <ul className="sidebar-list">
-          <li>Dashboard</li>
-          <li>Master</li>
-          <li>Categories</li>
-          <li>Product</li>
-          <li>Vendor</li>
-        </ul>
-      </div>
-    </>
+    <div className="SideBar">
+      <ul className="sidebar-list">
+        <li>Master</li>
+        <li>Categories</li>
+        <li>Product</li>
+        <li>Vendor</li>
+      </ul>
+    </div>
   );
 };
 
