dc3d9925685270cb41f67d638bf06e1b0f1880b1
Sagar3981 <sagar.pitta81@gmail.com>
2025-06-17 16:47:21 +0530

    dashboard changes done

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,10 +1,47 @@
+import { LuFolderCheck } from "react-icons/lu";
+import { PiUsersFill } from "react-icons/pi";
+import { HiCurrencyRupee } from "react-icons/hi2";
+import BasicPie from "./dashboard-pichart";
+
 const Dashboard = () => {
-    return (
-        <>
-            <div className="layout">
-                <h2>WELCOME TO ADMIN PANEL</h2>
+  return (
+    <>
+      <div className="d_main_container">
+        {" "}
+        <div className="dashboard_main_card">
+          <div className="dashboard_sec_card">
+            <div className="detailscard">
+              <div className="d-icon-container">
+                <LuFolderCheck className="d-icon-color" />
+              </div>
+              <div>
+                <h5>New Orders</h5>
+                <h4>1025</h4>
+              </div>
             </div>
-        </>
-    )
-}
-export default Dashboard
+            <div className="detailscard">
+              <div className="v-icon-container">
+                <PiUsersFill className="v-icon" />
+              </div>
+              <div>
+                <h5>Visitors</h5>
+                <h4>1025</h4>
+              </div>
+            </div>
+            <div className="detailscard">
+              <div className="rupe-icon-container">
+                <HiCurrencyRupee className="rupe-icon" />
+              </div>
+              <div>
+                <h5>Total Sales </h5>
+                <h4>RS 255543.00</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <BasicPie />
+      </div>
+    </>
+  );
+};
+export default Dashboard;
